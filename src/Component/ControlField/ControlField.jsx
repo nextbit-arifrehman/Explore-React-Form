572c6dcbdd350670cdc550fb59420ef8124e40cd
nextbit-arifrehman <asustufk513ea@gmail.com>
2025-04-19 02:42:30 +0600

    Control Form

--- src/Component/ControlField/ControlField.jsx
+++ src/Component/ControlField/ControlField.jsx
@@ -2,32 +2,46 @@ import React, { useState } from 'react';
 
 const ControlField = () => {
     const [password, setPassword] = useState('');
+    const [error,setError]=useState('');
 
     const handlePasswordOnChange = (e) => {
-        setPassword(e.target.value);
+        const newPassword = e.target.value;
+        setPassword(newPassword);
+        console.log(newPassword); // log each updated value
+
+        if(password.length <6 ){
+            setError("Password must be 6 charecters or longer.")
+        }
+        else{
+            setError('');
+        }
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("Form submitted");
-        console.log("Password:", password);
+        console.log("Final Password:", password);
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="name" placeholder="Name" /> <br />
+                <input type="Email" name="Email" placeholder="Email" /> <br />
                 <input
                     type="password"
                     onChange={handlePasswordOnChange}
-                    defaultValue={password}
+                    value={password}
                     required
                     placeholder="Password"
                 /><br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{color:'red'}}>
+                <small>{error}</small>
+            </p>
         </div>
     );
 };
 
 export default ControlField;
+
