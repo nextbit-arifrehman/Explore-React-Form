7ab824532744959c16d6ea7f0b3c721f374931bf
nextbit-arifrehman <asustufk513ea@gmail.com>
2025-04-19 15:55:56 +0600

    Context Emplement

--- src/Component/FamilyTree/RootGandpa/FamilyTree.jsx
+++ src/Component/FamilyTree/RootGandpa/FamilyTree.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Grandpa from './Grandpa';
 import Father from './Father';
 
+export const AssetContext = createContext('');
+
+
+
 const FamilyTree = () => {
     const asset ='diamond';
+    const newAsset ='Golden Horn'
     return (
         <div className='family-tree'>
            <h2>Family Tree</h2> 
-           <Grandpa asset={asset}></Grandpa>
+          <AssetContext.Provider value={newAsset}>
+          <Grandpa asset={asset}></Grandpa>
+          </AssetContext.Provider>
         </div>
     );
 };
--- src/Component/FamilyTree/Special/Special.jsx
+++ src/Component/FamilyTree/Special/Special.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from '../RootGandpa/FamilyTree';
 
 const Special= ({name ,asset}) => {
+  const newAsset = useContext(AssetContext);
+  console.log(newAsset,'new fire')
     return (
         <div>
           <h3>{name}</h3>  
           <p>Asset:{asset}</p>
+          <p>New Asset:{newAsset}</p>
         </div>
     );
 };
